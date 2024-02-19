028f552790b5b76e58fa6f4de5769a7ca67d56a2
Karol <93782725+petitess@users.noreply.github.com>
2024-02-19 16:37:33 +0100

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,10 @@
+sudo docker pull mcr.microsoft.com/azure-cli:2.57.0
+sudo docker ps
+sudo deocker run
+sudo docker image ls
+
+sudo docker run nginx -Pd  
+
+docker run -Pd nginxdemos/hello
+
+sudo docker run --name nginx -d -p 8080:80
